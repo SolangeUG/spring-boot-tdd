071676d7ce2aa954312993139c748ed3e429ba79
Solange U. Gasengayire <sgasengayire@gmail.com>
2019-07-13 15:49:13 +0100

    Add acceptance test for retrieving details of a car

--- src/test/java/org/java/training/tdd/AT_Application.java
+++ src/test/java/org/java/training/tdd/AT_Application.java
@@ -1,4 +1,32 @@
 package org.java.training.tdd;
 
+import org.java.training.tdd.model.Car;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static java.util.Objects.requireNonNull;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class AT_Application {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void return_a_car_details_response() {
+        ResponseEntity<Car> response = restTemplate.getForEntity("/cars/prius", Car.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(requireNonNull(response.getBody()).getName()).isEqualTo("prius");
+        assertThat(requireNonNull(response.getBody()).getType()).isEqualTo("hybrid");
+    }
 }
