7304348bff73a5cdf5769554a2ae2b6ac05244e9
Solange U. Gasengayire <sgasengayire@gmail.com>
2020-07-18 22:48:27 +0100

    Update seed data SQL script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
 -- Database: carsdb
 
 INSERT INTO cars(id, name, type) values (4, 'prius', 'hybrid');
-INSERT INTO cars(id, name, type) values (8, 'tesla', 'electric');
+INSERT INTO cars(id, name, type) values (8, 'toyota', 'petrol');
